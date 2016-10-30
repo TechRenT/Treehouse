44cd968b0cc60096e3d8d994075472d60dfc2a48
Renelle Tigue <titingrenelle@gmail.com>
2016-10-30 20:50:46 +0800

    Customizing Class-Based Views - Overriding Methods

--- Django_CBVs/djangoal/teams/views.py
+++ Django_CBVs/djangoal/teams/views.py
@@ -31,6 +31,11 @@ class TeamCreateView(CreateView):
     fields = ("name", "practice_location", "coach")
     model = models.Team
 
+    def get_initial(self):
+        initial = super().get_initial()
+        initial["coach"] = self.request.user.pk
+        return initial
+
 
 class TeamUpdateView(UpdateView):
     fields = ("name", "practice_location", "coach")
@@ -40,4 +45,8 @@ class TeamUpdateView(UpdateView):
 class TeamDeleteView(DeleteView):
     model = models.Team
     success_url = reverse_lazy("teams:list")
-    
+
+    def get_queryset(self):
+        if not self.request.user.is_superuser:
+            return self.model.objects.filter(coach=self.request.user)
+        return self.model.objects.all()
